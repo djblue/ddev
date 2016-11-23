0c4290806f32aad59a0f8b3b4d5bc644f6f7391b
Chris Badahdah <a.s.badahdah@gmail.com>
2016-11-23 16:44:56 -0700

    Started porting ddev over to a more open system
    - allow for easier extension using any language
    - allow easier leveraging of npm ecosystem
    - remove unused/unnecessary features
    - still need to port more existing features

---
+++ bin.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var spawn = require('child_process').spawn
+var join = require('path').join
+
+var fail = function (msg) {
+  console.error(msg)
+  process.exit(1)
+}
+
+var cmd = process.argv[2]
+var opts = process.argv.slice(3)
+
+if (cmd === '-v') {
+  cmd = 'version'
+}
+
+if (cmd === '-h') {
+  cmd = 'help'
+}
+
+if (cmd === undefined) {
+  fail('please specify command')
+}
+
+var extend = [
+  join(__dirname, 'commands'),
+  join(__dirname, 'node_modules', '.bin')
+]
+
+process.env.PATH = extend.concat(process.env.PATH.split(':')).join(':')
+
+var ps = spawn(cmd, opts, { stdio: 'inherit' })
+
+ps.on('error', function () {
+  fail('cannot find command: ' + cmd)
+})
+
+ps.on('exit', process.exit)
+
